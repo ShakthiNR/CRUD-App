788a748086f2a169dbb4864c16fde405e7a7d78b
shakthinr <shakthinaarayanan@gmail.com>
2023-09-08 23:11:48 +0530

    Unwanted files removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormComponent } from './components/form/Form.component';
+import { ButtonComponent } from './common/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ListsComponent,
+    FooterComponent,
+    FormComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
